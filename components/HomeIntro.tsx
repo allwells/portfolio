6ba7cfda7331ae80bdac2958637d129a21a07982
allwells <aleenfestus@gmail.com>
2021-06-05 02:49:48 +0100

    Add image to self intro

--- components/HomeIntro.tsx
+++ components/HomeIntro.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
 import styles from "../styles/HomeIntro.module.css";
 
 export default function HomeIntro() {
   return (
     <div className={styles.homeIntro}>
+      <div className={styles.profileParent}>
+        <div className={styles.profileBorder}>
+          <Image
+            className={styles.profile}
+            src="/profile.jpg"
+            width={250}
+            height={250}
+          />
+        </div>
+      </div>
       <p className={styles.name}>
         Hi! My name is <b>Allwell Onen</b>,
       </p>
