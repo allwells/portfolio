84a8b8aea3452c5bbea8f4ace53f4fb718604acd
allwells <aleenfestus@gmail.com>
2021-06-05 03:38:22 +0100

    Update home intro content

--- components/HomeIntro.tsx
+++ components/HomeIntro.tsx
@@ -4,31 +4,35 @@ import styles from "../styles/HomeIntro.module.css";
 export default function HomeIntro() {
   return (
     <div className={styles.homeIntro}>
+      {/* <h1>About Allwell</h1> */}
       <div className={styles.profileParent}>
-        <div className={styles.profileBorder}>
-          <Image
-            className={styles.profile}
-            src="/profile.jpg"
-            width={250}
-            height={250}
-          />
-        </div>
+        <Image
+          className={styles.profile}
+          src="/profile.jpg"
+          width={250}
+          height={250}
+        />
       </div>
-      <p className={styles.name}>
-        Hi! My name is <b>Allwell Onen</b>,
-      </p>
-      <p>
-        I'm a Software Developer and a Penetration Tester. You can contact me on
-        LinkedIn or Telegram. I'm a Software Developer and a Penetration Tester.
-        You can contact me on LinkedIn or Telegram. I'm a Software Developer and
-        a Penetration Tester. You can contact me on LinkedIn or Telegram.
-      </p>
-      <p>
-        I'm a Software Developer and a Penetration Tester. You can contact me on
-        LinkedIn or Telegram. I'm a Software Developer and a Penetration Tester.
-        You can contact me on LinkedIn or Telegram. I'm a Software Developer and
-        a Penetration Tester. You can contact me on LinkedIn or Telegram.
-      </p>
+      <div className={styles.introContent}>
+        <p className={styles.name}>
+          Hi! My name is <b>Allwell Onen</b>
+        </p>
+        <p>
+          This is just a dummy text so do not try to read it unless you want to
+          waste your time. This is just a dummy text so do not try to read it
+          unless you want to waste your time. This is just a dummy text so do
+          not try to read it unless you want to waste your time. This is just a
+          dummy text so do not try to read it unless you want to waste your
+          time. This is just a dummy text so do not try to read it unless you
+          want to waste your time. Thank you.
+        </p>
+        <p>
+          This is just a dummy text so do not try to read it unless you want to
+          waste your time. This is just a dummy text so do not try to read it
+          unless you want to waste your time. This is just a dummy text so do
+          not try to read it unless you want to waste your time. Thank you.
+        </p>
+      </div>
     </div>
   );
 }
