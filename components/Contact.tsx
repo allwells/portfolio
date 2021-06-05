570a68a54bbed00d6a4de588d6f6d4b501da7fe5
allwells <aleenfestus@gmail.com>
2021-06-05 23:13:34 +0100

    Update UI

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,69 +1,88 @@
 import styles from "../styles/Contact.module.css";
+import Image from "next/image";
 
-function Input({ placeholder, type, classname }) {
-  return <input placeholder={placeholder} type={type} className={classname} />;
+function Input({ type, classname }) {
+  return <input type={type} className={classname} />;
 }
 
 export default function Contact({ contact_me }) {
   return (
     <div className={styles.contact}>
-      <h1 id={contact_me}>Contact</h1>
+      <h1 id={contact_me}>Get in Touch</h1>
       <div className={styles.socialLinks}>
-        <a href="https://github.com/allwells" target="_blank">
-          <img
-            src="https://img.shields.io/badge/github-%2324292e.svg?&style=for-the-badge&logo=github&logoColor=white"
-            alt="github"
-          />
-        </a>
-        <a href="https://twitter.com/allwell_festus" target="_blank">
-          <img
-            src="https://img.shields.io/badge/twitter-%2300acee.svg?&style=for-the-badge&logo=twitter&logoColor=white"
-            alt="twitter"
-          />
-        </a>
-        <a href="https://linkedin.com/in/allwell-festus" target="_blank">
-          <img
-            src="https://img.shields.io/badge/linkedin-%231E77B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white"
-            alt="linkedin"
-          />
-        </a>
-        <a href="https://www.facebook.com/allwell.onen" target="_blank">
-          <img
-            src="https://img.shields.io/badge/facebook-%232E87FB.svg?&style=for-the-badge&logo=facebook&logoColor=white"
-            alt="facebook"
-          />
-        </a>
-        <a href="https://instagram.com/allwell_festus" target="_blank">
-          <img
-            src="https://img.shields.io/badge/instagram-%23000000.svg?&style=for-the-badge&logo=instagram&logoColor=white"
-            alt="instagram"
-          />
-        </a>
-        <a href="mailto: aleenfestus@gmail.com" target="_blank">
-          <img
-            src="https://img.shields.io/badge/gmail-%23000000.svg?&style=for-the-badge&logo=gmail&logoColor=white"
-            alt="gmail"
-          />
-        </a>
+        <div>
+          <a
+            className={styles.facebook}
+            href="https://facebook.com/allwell.onen"
+            target="_blank"
+          >
+            <Image src="/facebook-inactive.svg" width={10} height={10} />
+          </a>
+        </div>
+        <div>
+          <a
+            className={styles.linkedin}
+            href="https://linkedin.com/in/allwell-festus"
+            target="_blank"
+          >
+            <Image src="/linkedin-inactive.svg" width={10} height={10} />
+          </a>
+        </div>
+        <div>
+          <a
+            className={styles.telegram}
+            href="https://t.me/allwells"
+            target="_blank"
+          >
+            <Image src="/telegram-inactive.svg" width={10} height={10} />
+          </a>
+        </div>
+        <div>
+          <a
+            className={styles.whatsapp}
+            href="https://wa.me/+2348023495323"
+            target="_blank"
+          >
+            <Image src="/whatsapp-inactive.svg" width={10} height={10} />
+          </a>
+        </div>
+        <div>
+          <a
+            className={styles.gmail}
+            href="mailto:aleenfestus@gmail.com"
+            target="_blank"
+          >
+            <Image src="/gmail-inactive.svg" width={10} height={10} />
+          </a>
+        </div>
+      </div>
+      <div className={styles.or}>
+        <div></div>
+        <p>OR</p>
+        <div></div>
       </div>
       <div className={styles.sendEmail}>
-        <h1 className={styles.leaveMessage}>Leave a Message</h1>
         <div className={styles.namePanel}>
-          <Input
-            placeholder="Full name"
-            type="text"
-            classname={styles.nameField}
-          />
+          <p>
+            Name <span>*</span>
+          </p>
+          <Input type="text" classname={styles.nameField} />
         </div>
         <div className={styles.emailPanel}>
-          <Input
-            placeholder="Email address"
-            type="text"
-            classname={styles.emailField}
-          />
+          <p>
+            Email Address <span>*</span>
+          </p>
+          <Input type="text" classname={styles.emailField} />
+        </div>
+        <div className={styles.companyPanel}>
+          <p>Company</p>
+          <Input type="text" classname={styles.companyField} />
         </div>
         <div className={styles.messagePanel}>
-          <textarea placeholder="Message..." className={styles.messageField} />
+          <p>
+            Message <span>*</span>
+          </p>
+          <textarea className={styles.messageField} />
         </div>
         <div className={styles.sendButtonPanel}>
           <button className={styles.sendButton}>Send</button>
