b4e1f3339f51502eebb9a8ed948bbea3c441648e
allwells <aleenfestus@gmail.com>
2021-06-07 13:07:38 +0100

    Add projects and github links to works

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,15 +1,24 @@
 const theme = {
-  colors: {
+  lightTheme: {
     primary: "#22272e",
     border01: "#383b41",
     border02: "#6e7681",
     text: "#adbac7",
     textLight: "#adbac7aa",
     hoverBtn: "#444c56",
-    whitesmoke: "#f5f5f5",
+    blacksmoke: "#adbac7aa",
     textField: "#dbe5ee11",
+    programmingLanguage: {
+      java: "#b07219",
+      javascript: "#f1e05a",
+      typescript: "#2b7489",
+      python: "#3572a5",
+      php: "#4f5d95",
+      shell: "#89e051",
+      r: "#198ce7",
+    },
   },
-  lightTheme: {
+  lighTheme: {
     primary: "#f5f5f5",
     border01: "#dfdfdf",
     border02: "#ababab",
@@ -18,10 +27,21 @@ const theme = {
     hoverBtn: "#444c56dd",
     blacksmoke: "#444c56",
     textField: "#dbe5ee22",
+    programmingLanguage: {
+      java: "#b07219",
+      javascript: "#f1e05a",
+      typescript: "#2b7489",
+      python: "#3572a5",
+      php: "#4f5d95",
+      shell: "#89e051",
+      r: "#198ce7",
+    },
   },
-  tablet: "760px",
-  ipad: "860px",
-  iphonex: "460px",
+  screenSize: {
+    tablet: "760px",
+    ipad: "860px",
+    iphonex: "460px",
+  },
 };
 
 export default theme;
