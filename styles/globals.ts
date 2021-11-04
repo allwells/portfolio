f508f814be3d38b407c4349da09b9777ed708799
Allwell Onen <aleenfestus@gmail.com>
2021-11-04 11:21:17 +0100

    Update fonts
    Signed-off-by: Allwell Onen <aleenfestus@gmail.com>

--- styles/globals.ts
+++ styles/globals.ts
@@ -3,9 +3,7 @@ export const GlobalStyles = createGlobalStyle`
   html, body {
     padding: 0;
     margin: 0;
-    // font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-
-  font-family: Comfortaa;
+    font-family: -apple-system, BlinkMacSystemFont, Comfortaa;
   }
   
 
