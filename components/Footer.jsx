bba1c822268582eacc235824c017cb1c83fcb348
Allwell Onen <aleenfestus@gmail.com>
2022-08-30 09:33:22 +0100

    Update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="flex max-w-[1000px] py-3 border-t border-neutral-700 text-[10px] items-center justify-center w-full text-center text-white font-normal tracking-wider">
+    <footer className="flex max-w-[1000px] py-3 border-t border-neutral-700 text-[10px] items-center justify-center w-full text-center text-neutral-300 font-normal tracking-wider">
       Built with
       <a
         target="_blank"
         rel="noreferrer"
         href="https://nextjs.org"
-        className="mx-1 text-blue-500 hover:underline"
+        className="mx-1 text-white hover:underline font-bold"
       >
         Next.js
       </a>
@@ -14,20 +14,30 @@ export default function Footer() {
       <a
         target="_blank"
         rel="noreferrer"
-        href="https://typescriptlang.org"
-        className="mx-1 text-blue-700 hover:underline"
+        href="https://www.typescriptlang.org"
+        className="mx-1 text-blue-700 hover:underline font-bold"
       >
         Typescript
+      </a>
+      ,
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://mantine.dev"
+        className="mx-1 text-blue-500 hover:underline font-bold"
+      >
+        Mantine
       </a>
       and
       <a
         target="_blank"
         rel="noreferrer"
         href="https://tailwindcss.com"
-        className="ml-1 text-sky-400 hover:underline"
+        className="ml-1 text-sky-400 hover:underline font-bold"
       >
         tailwindcss
       </a>
+      .
     </footer>
   );
 }
